2dbb03351f7bb90e8c8c6a5577e270912b7262d8
Ni <nadedashraf@gmail.com>
2026-09-20 18:26:47 +0300

    Fix SplashScreen graphicsLayer import

--- nexis-android/app/src/main/java/com/nadidstudio/nexis/ui/screens/splash/SplashScreen.kt
+++ nexis-android/app/src/main/java/com/nadidstudio/nexis/ui/screens/splash/SplashScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.graphicsLayer
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight

b4d82882e988e01cf58870270b8f1d46825169f0
Ni <nadedashraf@gmail.com>
2026-09-23 02:03:19 +0300

    fix: add missing CompositionLocalProvider import

--- nexis-android/app/src/main/java/com/nadidstudio/nexis/ui/screens/Common.kt
+++ nexis-android/app/src/main/java/com/nadidstudio/nexis/ui/screens/Common.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
